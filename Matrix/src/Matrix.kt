8835f06685960ffad4f92e130b0e3ddf1aacc307
ainurbl <ainur19dota2@mail.ru>
2020-11-11 21:43:45 +0300

    Added some basic function of Matrix + tests

---
+++ Matrix/src/Matrix.kt
@@ -0,0 +1,105 @@
+import java.lang.Math.sqrt
+
+data class Matrix(private val rows: Int, private val cols: Int) {
+    private val arr: MutableList<MutableList<Double>> = mutableListOf()
+
+    init {
+        for (i in 0 until rows) {
+            arr.add(mutableListOf())
+            for (j in 0 until cols) {
+                arr[i].add(0.0)
+            }
+        }
+    }
+
+    fun transpose(): Matrix {
+        val returnMatrix = Matrix(cols, rows)
+        for (i in 0 until rows) {
+            for (j in 0 until cols) {
+                returnMatrix[j][i] = arr[i][j]
+            }
+        }
+        return returnMatrix
+    }
+
+    fun F(): Double {
+        var result = 0.0
+        for (i in 0 until rows) {
+            for (j in 0 until cols) {
+                result += arr[i][j] * arr[i][j]
+            }
+        }
+        return sqrt(result)
+    }
+
+    private fun cuteSeparator() {
+        for (i in 0..20) print("=")
+        println()
+    }
+
+    fun print() {
+        cuteSeparator()
+        for (i in 0 until rows) {
+            if (i > 0) {
+                println()
+            }
+            for (j in 0 until cols) {
+                print("${arr[i][j]} ")
+            }
+        }
+        println()
+        cuteSeparator()
+    }
+
+    operator fun get(y: Int, x: Int) = arr[y][x]
+
+    operator fun get(y: Int) = arr[y]
+
+    operator fun set(y: Int, x: Int, newValue: Double) {
+        arr[y][x] = newValue
+    }
+
+    operator fun unaryMinus(): Matrix {
+        val returnMatrix = this.copy()
+        for (i in 0 until rows) {
+            for (j in 0 until cols) {
+                returnMatrix[i][j] = -arr[i][j]
+            }
+        }
+        return returnMatrix
+    }
+
+    operator fun plus(otherMatrix: Matrix): Matrix {
+        assert(rows == otherMatrix.rows && cols == otherMatrix.cols)
+        val returnMatrix = this.copy()
+        for (i in 0 until rows) {
+            for (j in 0 until cols) {
+                returnMatrix[i][j] = arr[i][j] + otherMatrix[i][j]
+            }
+        }
+        return returnMatrix
+    }
+
+    operator fun times(otherMatrix: Matrix): Matrix {
+        assert(cols == otherMatrix.rows)
+        val returnMatrix = Matrix(rows, otherMatrix.cols)
+        for (i in 0 until returnMatrix.rows) {
+            for (j in 0 until returnMatrix.cols) {
+                for (k in 0 until cols) {
+                    returnMatrix[i][j] += arr[i][k] * otherMatrix[k][j]
+                }
+            }
+        }
+        return returnMatrix
+    }
+
+    companion object {
+        fun I(n: Int): Matrix {
+            val returnMatrix = Matrix(n, n)
+            for (i in 0 until n) {
+                returnMatrix[i][i] = 1.0
+            }
+            return returnMatrix
+        }
+    }
+}
---
+++ Test/src/MatrixTest.kt
@@ -0,0 +1,122 @@
+import org.junit.jupiter.api.Assertions.*
+import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.assertThrows
+import java.lang.Math.abs
+
+internal class MatrixTest {
+
+    private val accuracy = 0.000001
+
+    private fun equals(a: Double, b: Double): Boolean {
+        return abs(a - b) < accuracy
+    }
+
+    @Test
+    fun `matrix unary minus`() {
+        val rows = 2
+        val cols = 3
+        val mat = Matrix(rows, cols)
+        for (i in 0 until rows) {
+            for (j in 0 until cols) {
+                mat[i][j] = i * j + 2 * i + 2 * j + 0.0
+            }
+        }
+        val minusMat = -mat
+        for (i in 0 until rows) {
+            for (j in 0 until cols) {
+                assert(equals(mat[i][j], -minusMat[i][j]))
+            }
+        }
+    }
+
+    @Test
+    fun `matrix plus`() {
+        val rows = 3
+        val cols = 4
+        val a = Matrix(rows, cols)
+        val b = Matrix(rows, cols)
+        for (i in 0 until rows) {
+            for (j in 0 until cols) {
+                a[i][j] = i * j + 2 * i + 2 * j + 0.0
+                b[i][j] = 62 * i + 443.0 * j + i * j
+            }
+        }
+        val c = a + b
+        for (i in 0 until rows) {
+            for (j in 0 until cols) {
+                assert(equals(a[i][j] + b[i][j], c[i][j]))
+            }
+        }
+    }
+
+    @Test
+    fun `matrix mult 1`() {
+        val rows = 2
+        val cols = 2
+        val a = Matrix(rows, cols)
+        val b = Matrix(rows, cols)
+        a[0][0] = 1.0
+        b[0][0] = 3.0
+        a[0][1] = 2.0
+        b[0][1] = -3.0
+        a[1][0] = 3.0
+        b[1][0] = 2.0
+        a[1][1] = -4.0
+        b[1][1] = 1.0
+        val c = a * b
+        assert(equals(c[0][0], 7.0))
+        assert(equals(c[0][1], -1.0))
+        assert(equals(c[1][0], 1.0))
+        assert(equals(c[1][1], -13.0))
+    }
+
+    @Test
+    fun `matrix mult 2`() {
+        val rows = 2
+        val cols = 3
+        val a = Matrix(rows, cols)
+        val b = Matrix(cols, rows)
+        for (i in 0 until rows) {
+            for (j in 0 until cols) {
+                a[i][j] = i * 3 + j + 1.0
+                b[j][i] = -i * 3 - j - 1.0
+            }
+        }
+        b[1][0] = -b[1][0]
+        b[1][1] = -b[1][1]
+        val c = a * b
+        assert(equals(c[0][0], -6.0))
+        assert(equals(c[0][1], -12.0))
+        assert(equals(c[1][0], -12.0))
+        assert(equals(c[1][1], -27.0))
+    }
+
+    @Test
+    fun `matrix transpose 1`() {
+        val rows = 2
+        val cols = 2
+        val a = Matrix(rows, cols)
+        a[0][0] = 1.0
+        a[1][1] = 1.0
+        a[1][0] = 5.0
+        a[0][1] = -2.0
+        val b = a.transpose()
+        assert(equals(b[0][0], 1.0))
+        assert(equals(b[0][1], 5.0))
+        assert(equals(b[1][0], -2.0))
+        assert(equals(b[1][1], 1.0))
+    }
+
+    @Test
+    fun `matrix transpose 2`() {
+        val rows = 2
+        val cols = 1
+        val a = Matrix(rows, cols)
+        a[0][0] = 4.0
+        a[1][0] = 5.0
+        val b = a.transpose()
+        assert(equals(b[0][0], a[0][0]))
+        assert(equals(b[0][1], a[1][0]))
+    }
+
+}
