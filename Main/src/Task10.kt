4cadcc425939cd9649939740fb5916a616d2cbc2
ainurbl <ainur19dota2@mail.ru>
2020-12-13 23:57:21 +0300

    Task10: from O(n^3) to O(n^2)

--- Main/src/Task10.kt
+++ Main/src/Task10.kt
@@ -2,11 +2,11 @@ import Matrix.Companion.I
 import java.math.BigDecimal
 import java.math.RoundingMode
 
-class Task10: Task {
+class Task10 : Task {
     var n = 0
-    var A = Matrix(0,0)
+    var A = Matrix(0, 0)
     var eps = BigDecimal.ZERO.setScale(Precision.scale, RoundingMode.HALF_UP)
-    var vs = Matrix(0,0)
+    var vs = Matrix(0, 0)
 
     override fun input() {
         n = read.nextInt()
@@ -23,14 +23,16 @@ class Task10: Task {
         assert(A.isTridiagonal(eps))
         val engine = Task4(true)
         engine.n = n
-        var RQ = Matrix(n,n)
+        var RQ = Matrix(n, n)
         engine.A = A
         vs = I(n)
 
         do {
 //            engine.A.print()
             engine.execute()
-            RQ = engine.R * engine.Q
+            RQ = fastMult(engine.R, engine.Q)
+//            engine.Q.print()
+//            engine.R.print()
 //            (engine.Q * engine.R ).print()
             vs *= engine.Q
             engine.A = RQ
@@ -38,6 +40,23 @@ class Task10: Task {
         } while (!engine.A.allCirclesAreLessThan(eps))
         A = engine.A
         return true
+    }
+
+    private fun fastMult(r: Matrix, q: Matrix): Matrix {
+        val retMatrix = Matrix(n, n)
+        for (i in 0 until n) {
+            for (j in 0 until n) {
+                var adding = q[i][j] * r[i][i]
+                if (i != n - 1) {
+                    adding += r[i][i + 1] * q[i + 1][j]
+                }
+                if(i != n - 1 && i != n - 2) {
+                    adding += r[i][i + 2] * q[i + 2][j]
+                }
+                retMatrix[i][j] = adding.setScale(Precision.scale, RoundingMode.HALF_UP)
+            }
+        }
+        return retMatrix
     }
 
     override fun output() {
